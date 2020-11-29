7dc25be2625f5d4e00c23b16f4c31f1e5e36b044
Alireza <a.helali1995@gmail.com>
2020-11-29 12:49:32 +0330

    project url set

--- geoproject/urls.py
+++ geoproject/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from shops.views import ShowShop
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', ShowShop.as_view())
 ]
