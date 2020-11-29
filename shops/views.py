ce24693ab8f6511e75367f6472f95964169f763a
Alireza <a.helali1995@gmail.com>
2020-11-29 12:50:45 +0330

    wrote views with class base view

--- shops/views.py
+++ shops/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from shops.models import Shop
+from django.contrib.gis.geos import fromstr, Point
+from django.contrib.gis.db.models.functions import Distance
 
 # Create your views here.
+
+
+longitude = -80.191788
+latitude = 25.761681
+
+user_location = Point(longitude, latitude, srid=4326)
+
+
+class ShowShop(ListView):
+    template_name = "shops.html"
+    model = Shop
+    context_object_name = 'shops'
+    queryset = Shop.objects.annotate(distance=Distance('location', user_location)).order_by('distance')[0:6]
